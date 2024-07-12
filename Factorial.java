d22fcb86fadbeacba92c5719283b1408b6c6919f
Achala Punsara <92708168+AchalaHM@users.noreply.github.com>
2024-07-12 11:24:58 +0530

    Method to find factorial of number
    Without recursion

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial : ");
+        int n = scanner.nextInt();
+        int result = 1;
+
+        for(int i = 1; i <= n ; i++){
+            result = result * i;
+        }
+
+        System.out.println("Factorial of "+ n+ " is "+ result);
+    }
+}
